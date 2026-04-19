97253417830a3793de18906b558c829cc53e1dea
mhj <mhjmhj2002@gmail.com>
2026-04-19 12:26:37 -0300

    chore: initial project setup with base structure and agent rules

---
+++ src/main/java/com/agenticia/msorder/AgenticIaMsOrderApplication.java
@@ -0,0 +1,13 @@
+package com.agenticia.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenticIaMsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenticIaMsOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenticia/msorder/AgenticIaMsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenticia.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenticIaMsOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
